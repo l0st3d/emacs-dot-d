a5ae2cccadc9b16bb85094097badb9c6c0db245e
Edward Bowler <edward.bowler@skybettingandgaming.com>
2015-11-14 12:16:21 +0000

    useful keybindings and don't complie profiles.clj

--- local-lisp/clojure-setup.el
+++ local-lisp/clojure-setup.el
@@ -1,4 +1,4 @@
-;;; clojure-setup.el --- Ed's setup
+;;; clojure-setup.el --- Ed's clojure setup
 
 ;;; Commentary:
 
@@ -10,17 +10,28 @@
 
 ;; (setq cider-prompt-save-file-on-load 'always-save)u
 
-(add-hook 'after-save-hook (lambda ()
-			     (when (and (eq major-mode 'clojure-mode)
-					(not (string-match ".*/project\\.clj$" (buffer-file-name)))
-					(cider-connected-p))
-			       (cider-load-buffer))))
+(defun ed-clojure/compile-after-save ()
+  "Compile after save."
+  (when (and (eq major-mode 'clojure-mode)
+	     (or (not (string-match ".*/project\\.clj$" (buffer-file-name)))
+		 (not (string-match ".*/profiles\\.clj$" (buffer-file-name))))
+	     (cider-connected-p))
+    (cider-load-buffer)))
+
+(add-hook 'after-save-hook 'ed-clojure/compile-after-save)
 (require 'paredit)
 
-(defun ed/clojure-bind-keys ()
-  (local-set-key (kbd "C-c <C-BACKSPACE>") 'paredit-raise-sexp))
+(defun ed-clojure/bind-keys ()
+  "Bind keys."
+  (define-key clojure-mode-map (kbd "C-c <C-backspace>") 'paredit-raise-sexp)
+  (define-key clojure-mode-map (kbd "C-c DEL") 'paredit-splice-sexp-killing-backward)
+  (define-key clojure-mode-map (kbd "C-c C-9") 'paredit-backward-slurp-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-0") 'paredit-forward-slurp-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-(") 'paredit-backward-barf-sexp)
+  (define-key clojure-mode-map (kbd "C-c C-)") 'paredit-forward-barf-sexp)
+  (define-key clojure-mode-map (kbd "C-c \"") 'clojure-toggle-keyword-string))
 
-(add-hook 'cider-mode-hook 'ed/clojure-bind-keys)
+(add-hook 'clojure-mode-hook 'ed-clojure/bind-keys)
 
 ;; (provide 'clojure-setup)
 ;;; clojure-setup.el ends here
