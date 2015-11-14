9898a269e46cc449d4a0521d50441a64371fbd8b
Edward Bowler <edward.bowler@skybettingandgaming.com>
2015-11-14 12:09:53 +0000

    added

--- custom.el
+++ custom.el
@@ -8,6 +8,8 @@
    (quote
     (ed-java/check-with-javac ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck groovy haml handlebars haskell-ghc haskell-hlint html-tidy jade javascript-jshint javascript-eslint javascript-gjslint javascript-jscs javascript-standard json-jsonlint less luacheck lua perl perl-perlcritic php php-phpmd php-phpcs puppet-parser puppet-lint python-flake8 python-pylint python-pycompile r-lintr racket rpm-rpmlint rst rst-sphinx ruby-rubocop ruby-rubylint ruby ruby-jruby rust sass scala scala-scalastyle scss-lint scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim sql-sqlint tex-chktex tex-lacheck texinfo verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby)))
  '(global-company-mode t)
+ '(indent-tabs-mode nil)
+ '(sentence-end-double-space nil)
  '(sql-informix-program "/Users/ebo03/Apps/informix/dbaccess.sh")
  '(sql-server ""))
 (custom-set-faces
